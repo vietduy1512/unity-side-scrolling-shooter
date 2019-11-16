fb13807566cb6924e62fb4c457bbf340024acddf
Duy <38127890+vietduy1512@users.noreply.github.com>
2019-11-16 16:33:27 +0700

    Implement Enemy Path

---
+++ Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPath : MonoBehaviour
+{
+    [SerializeField]
+    List<Transform> waypoints;
+
+    [SerializeField]
+    float moveSpeed = 2f;
+
+    int waypointsIndex = 0;
+
+    void Start()
+    {
+        transform.position = waypoints[waypointsIndex].transform.position;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointsIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointsIndex].transform.position;
+            transform.position = Vector2.MoveTowards(transform.position,
+                                                    targetPosition,
+                                                    moveSpeed * Time.deltaTime);
+
+            if (transform.position == targetPosition)
+            {
+                waypointsIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
