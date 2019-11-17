9541226a54902f4558ac2d56478d1bf65d52850e
Duy <38127890+vietduy1512@users.noreply.github.com>
2019-11-17 13:31:26 +0700

    Implemment enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+
+    int startingWave = 0;
+
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnEnemies(currentWave));
+    }
+
+    private IEnumerator SpawnEnemies(WaveConfig waveConfig)
+    {
+        for (int i = 0; i < waveConfig.GetNumberOfEnemies(); i++)
+        {
+            Instantiate(waveConfig.GetEnemyPrefab(),
+               waveConfig.GetWaypoints()[0].transform.position,
+               Quaternion.identity);
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+}
