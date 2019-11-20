0f05b04613c42ecb78549375e630d1dc41e40204
Duy <38127890+vietduy1512@users.noreply.github.com>
2019-11-20 22:30:38 +0700

    Implement scrolling background

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.1f;
+
+    Material material;
+    Vector2 offset;
+
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        offset = new Vector2(backgroundScrollSpeed, 0);
+    }
+
+    void Update()
+    {
+        material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
