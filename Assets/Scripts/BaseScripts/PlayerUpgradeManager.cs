ef86cf743aee0ad46072a8de2813dbd783357ff1
Duy <38127890+vietduy1512@users.noreply.github.com>
2019-12-14 19:41:56 +0700

    Fix Updare feature

--- Assets/Scripts/BaseScripts/PlayerUpgradeManager.cs
+++ Assets/Scripts/BaseScripts/PlayerUpgradeManager.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerUpgradeManager : MonoBehaviour
 {
+    [SerializeField] GameObject upgradeScreen;
+
     [SerializeField] float initFireRate = 0.3f;
     [SerializeField] float initMoveSpeed = 8f;
     [SerializeField] float initHealth = 5;
@@ -16,6 +19,11 @@ public class PlayerUpgradeManager : MonoBehaviour
     [SerializeField] int maxBarrettsOffset = 1;
     [SerializeField] int maxShieldsOffset = 1;
 
+    public static int fireRateCount = 0;
+    public static int moveSpeedCount = 0;
+    public static int healthCount = 0;
+    public static int maxBarrettsCount = 0;
+    public static int maxShieldsCount = 0;
 
     public static float fireRate;
     public static float moveSpeed;
@@ -25,35 +33,68 @@ public class PlayerUpgradeManager : MonoBehaviour
 
     private void Awake()
     {
-        fireRate = initFireRate;
-        moveSpeed = initMoveSpeed;
-        health = initHealth;
-        maxBarretts = initMaxBarretts;
-        maxShields = initMaxShields;
+        fireRate = initFireRate + fireRateUpgradeOffset * fireRateCount;
+        moveSpeed = initMoveSpeed + moveSpeedUpgradeOffset * moveSpeedCount;
+        health = initHealth + healthUpgradeOffset * healthCount;
+        maxBarretts = initMaxBarretts + maxBarrettsOffset * maxBarrettsCount;
+        maxShields = initMaxShields + maxBarrettsOffset * maxBarrettsCount;
+        InitUpgradeUI();
     }
 
-    public void upgradeFireDelay()
+    private void Start()
     {
+    }
+
+    public void upgradeFireRate()
+    {
+        fireRateCount++;
+        ChangeLevelText("FireRateLevelCount", fireRateCount);
         fireRate += fireRateUpgradeOffset;
     }
 
     public void upgradeMoveSpeed()
     {
+        moveSpeedCount++;
+        ChangeLevelText("MoveSpeedLevelCount", moveSpeedCount);
         moveSpeed += moveSpeedUpgradeOffset;
     }
 
     public void upgradeHealth()
     {
+        healthCount++;
+        ChangeLevelText("HealthLevelCount", healthCount);
         health += healthUpgradeOffset;
     }
 
     public void upgradeBarrett()
     {
+        maxBarrettsCount++;
+        ChangeLevelText("BarrettLevelCount", maxBarrettsCount);
         maxBarretts += maxBarrettsOffset;
     }
 
     public void upgradeMaxShields()
     {
+        maxShieldsCount++;
+        ChangeLevelText("ShieldLevelCount", maxShieldsCount);
         maxShields += maxShieldsOffset;
+    }
+
+    private void ChangeLevelText(string name, int count)
+    {
+        GameObject.Find(name).GetComponent<Text>().text = "Level: " + count;
+    }
+
+    void InitUpgradeUI()
+    {
+        upgradeScreen.SetActive(true);
+
+        ChangeLevelText("FireRateLevelCount", fireRateCount);
+        ChangeLevelText("MoveSpeedLevelCount", moveSpeedCount);
+        ChangeLevelText("HealthLevelCount", healthCount);
+        ChangeLevelText("BarrettLevelCount", maxBarrettsCount);
+        ChangeLevelText("ShieldLevelCount", maxShieldsCount);
+
+        upgradeScreen.SetActive(false);
     }
 }
